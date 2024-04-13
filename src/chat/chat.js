f44942abbc44933a605c8f2bb387a982e2fe2685
Mrunal Nirajkumar Shah <mrunalnshah@protonmail.com>
2024-04-13 18:23:43 +0530

    .sent and .receive added for css purpose
    we can do css on .sent and .receive messages individually now

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -1,14 +1,16 @@
-window.onload = document.getElementById("profile-username").innerHTML = localStorage.getItem("userName");
+window.onload = document.getElementById("profile-username").innerHTML =
+  localStorage.getItem("userName");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBumQaN29IZF8LqvB3kqUIgF7v1r5m2Hv0",
-    authDomain: "chatweb-awful.firebaseapp.com",
-    databaseURL: "https://chatweb-awful-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "chatweb-awful",
-    storageBucket: "chatweb-awful.appspot.com",
-    messagingSenderId: "693279532996",
-    appId: "1:693279532996:web:cf6404be59950947e26691"
-  };
+  apiKey: "AIzaSyBumQaN29IZF8LqvB3kqUIgF7v1r5m2Hv0",
+  authDomain: "chatweb-awful.firebaseapp.com",
+  databaseURL:
+    "https://chatweb-awful-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chatweb-awful",
+  storageBucket: "chatweb-awful.appspot.com",
+  messagingSenderId: "693279532996",
+  appId: "1:693279532996:web:cf6404be59950947e26691",
+};
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
@@ -39,8 +41,12 @@ const fetchChat = db.ref("messages/");
 
 fetchChat.on("child_added", function (snapshot) {
   const messages = snapshot.val();
-  const message = `<li class= message ${
+  const message = `<li class="message ${
     username === messages.username ? "sent" : "receive"
-  }><img src="user-icon-png.png"><span>${messages.username}: <br></span>${messages.message}</li>`;
+  }">
+  <img src="user-icon-png.png"><span>${messages.username}: <br></span>${
+    messages.message
+  }</li>`;
+
   document.getElementById("messages").innerHTML += message;
 });
