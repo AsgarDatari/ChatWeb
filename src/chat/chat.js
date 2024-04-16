a39a07e5abc15956f61b38ed48399b7502f23467
Aaquif Qureshi <aaquifkureshi04.ak@gmail.com>
2024-04-16 10:11:05 +0530

    Time Update

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -35,41 +35,44 @@ document.getElementById("message-btn").addEventListener("click", sendMessage);
 function sendMessage(e) {
   e.preventDefault();
 
-  const timestamp = Date.now();
   const messageInput = document.getElementById("message-box");
   const message = messageInput.value;
 
-  if (message !== "") {
+  if (message.trim() !== "") {
+    const timestamp = Date.now();
     messageInput.value = "";
 
     db.ref("messages/" + timestamp).set({
       username,
       email,
       message,
+      timestamp: timestamp,
     });
   }
 
-  document
-    .getElementById("messages")
-    .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+  document.getElementById("messages").scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
 }
 
+// Fetch messages from Firebase Realtime Database
 const fetchChat = db.ref("messages/");
 
-fetchChat.on("child_added", function (snapshot) {
-  {
-    const messages = snapshot.val();
-    const message = `<li class="message ${
-      username === messages.username ? "sent" : "receive"
-    }">
-    <img src="https://api.dicebear.com/8.x/initials/svg?seed=${
-      messages.username
-    }?backgroundColor=b6e3f4,c0aede,d1d4f9"> <span> <b> ${
-      messages.username
-    } </b><br> </span>${messages.message}</li>`;
-    document.getElementById("messages").innerHTML += message;
-  }
-  document
-    .getElementById("messages")
-    .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+fetchChat.on("child_added", function(snapshot) {
+  const messages = snapshot.val();
+  const messageTime = new Date(messages.timestamp);
+  const formattedTime = messageTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+  const message = `<li class="message ${
+    username === messages.username ? "sent" : "receive"
+  }">
+    <img src="https://api.dicebear.com/8.x/initials/svg?seed=${messages.username}?backgroundColor=b6e3f4,c0aede,d1d4f9">
+    <span>
+      <b>${messages.username}</b><br>
+      ${messages.message}<br>
+      <small>${formattedTime}</small>
+    </span>
+  </li>`;
+
+  document.getElementById("messages").innerHTML += message;
+
+  document.getElementById("messages").scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
 });
